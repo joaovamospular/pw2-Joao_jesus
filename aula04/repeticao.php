cc48f730c71b8857ce437fc057340a3aeab1d9c4
João Vitor <Joaodraw5@gmail.com>
2025-03-19 10:28:35 -0300

    Aula 4 sobre repetição

---
+++ aula04/repeticao.php
@@ -0,0 +1,25 @@
+
+<?php
+$i = 1;
+while ($i <=6) {
+  echo $i;
+  echo "<br>";
+  $i++;
+}
+echo "<br>";
+
+$i = 1;
+
+do {
+    echo $i;
+    $i++;
+
+} while ($i < 6);
+
+echo "---------------<br>";
+echo "<br>";
+
+for ($x = 0; $x <= 10; $x++) {
+    echo "O ´número é: $x <br>";
+  }
+?>
