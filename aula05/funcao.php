c1ab1e18bd2ebfb2ce4ddc78ace990a048ad6925
Joao Vitor <Joaodraw5@gmail.com>
2025-04-09 10:32:17 -0300

    Aula 05 sobre funções

---
+++ aula05/funcao.php
@@ -0,0 +1,16 @@
+<?php
+
+function myMessage() {
+    echo "Hello world! <br>";
+  }
+  
+  myMessage();
+  myMessage();
+
+  function Soma($n1, $n2, ){
+    echo $n1 + $n2, "<br>";
+  }
+
+  Soma(3, 4);
+  Soma(10, 7)
+?>
