37567e00b4f79a36b7cb69716ba9ebcfd1e0b20d
Joao Vitor <Joodraw5@gmail.com>
2025-06-18 10:14:50 -0300

    adicionando aulas

---
+++ aula03/variaveis.php
@@ -0,0 +1,12 @@
+<?php
+$texto = "isso é um texto";
+$n1 = 2;
+$n2 = 3.14;
+$valor = null; 
+
+echo $texto;
+echo "<br>";
+echo gettype ($texto), "<br>";
+echo gettype ($n1), "<br>";
+echo gettype ($n2), "<br>";
+echo gettype ($valor);
